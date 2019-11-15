753bd360734d2b71f0617c4bd726bbee306da0eb
emmosley <55521788+emmosley@users.noreply.github.com>
2019-11-14 19:55:48 -0500

    fixes for lab

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let play = prompt("Do you want to play?", "type yes or no")
 
-if (play === 'yes') {
+if (play.toLowerCase() === 'yes' ) {
     let username = prompt ("Name your character")
     let grantHP = 10
     let playerHP = 40
     let numberOfWins = 0
-    let grantLoses = 0
     while(playerHP > 0 && numberOfWins < 3){ 
-      randomNumber = Math.floor(Math.random() + 1); 
+      randomNumber = Math.floor(Math.random() * 2) + 1; 
         grantHP -= randomNumber
         playerHP -= randomNumber
     //print user points left
@@ -15,15 +14,17 @@ if (play === 'yes') {
     console.log(`Grant the Mighty Chicken has ${grantHP} health points left.`);
     //if grant loses game, adds one loss and one win to player starts grant over at 10
       if (grantHP <= 0) {
-        grantLoses++;
         numberOfWins++;
         grantHP = 10
-        console.log(`Grant the Mighty Chicken has been defeated and ${username} has ${numberOfWins} wins`);
-      } else if (grantLoses < 3 && playerHP === 0) {
-        console.log("Grant the Mighty Chicken wins the battle!")
+        console.log(`Grant has been defeated and ${username} has ${numberOfWins} wins`);
+      } else if (numberOfWins < 3 && playerHP === 0) {
+        console.log(`${username} hes been defeated and Grant wins the round!`)
       };
 
-    if (grantLoses === 3 && playerHP >= 0) {
-        console.log(`${username} wins the battle!`)
-     };
+    if (numberOfWins === 3 && playerHP >= 0) {
+        console.log(`${username} wins the battle`)
+     } else if (playerHP <= 0 ) {
+       console.log (`Grant the Mighty Chicken wins the battle!`)
+     }
+     ;
     }}
